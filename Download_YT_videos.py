70a837178d44937eecb54ce96ee35908dd1415c3
Tiago Leite <tiagoleite2001@outlook.com>
2023-02-09 13:30:13 +0000

    Option to download the fastest or highest quality

--- Download_YT_videos.py
+++ Download_YT_videos.py
@@ -1,26 +1,53 @@
+
 from pytube import YouTube
 import subprocess
 import os
 
+def deleteFile(path):
+    os.remove(path)
+
 def download_video(url):
+    audio_path = "audio.mp4"
+    video_path = "video.mp4"
+
     yt = YouTube(url)
     videos = yt.streams
-    
+
     audio = videos.filter(mime_type='audio/mp4').order_by('abr').desc().first()
 
     original_filename = audio.default_filename
 
+    video_non_progressive = videos.filter(mime_type='video/mp4',progressive=False, file_extension='mp4').order_by('resolution').desc().first()
+    video_progressive = videos.filter(mime_type='video/mp4',progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+
     print(original_filename)
 
-    audio_path = "audio.mp4"
-    audio.download(filename=audio_path)
-    
-    video = videos.filter(mime_type='video/mp4',progressive=False, file_extension='mp4').order_by('resolution').desc().first()
-    print("Highest resolution available non progressive video: ", video.resolution)
-    video_path = "video.mp4"
-    video.download(filename=video_path)
-    
-    join_audio_video(video_path, audio_path, original_filename)
+    if(video_progressive.resolution == video_non_progressive.resolution):
+        print("\nHighest resolution available: ", video_progressive.resolution)
+        video_progressive.download()
+
+    else:
+
+        print("\n(Default/Fatest) 0 - Highest resolution available (progressive video): ", video_progressive.resolution)
+        print("(Slowest) 1  - Highest resolution available (non progressive video): ", video_non_progressive.resolution)
+
+
+        choice = input("Choose desired resolution: \n")
+
+        if(choice == 1):
+
+            video_non_progressive.download(filename=video_path)
+            audio.download(filename=audio_path)
+
+            join_audio_video(video_path, audio_path, original_filename)
+
+            deleteFile(video_path)
+            deleteFile(audio_path)
+
+        else:
+            video_progressive.download()
+
+
 
 def join_audio_video(video_file, audio_file, output_file):
     command = [
@@ -39,10 +66,7 @@ url = input("Enter the URL of the video you want to download: ")
 download_video(url)
 print("Video was downloaded sucessfully!")
 
-# delete the audio file
-os.remove("audio.mp4")
-# delete the video file
-os.remove("video.mp4")
+
 
 
 
